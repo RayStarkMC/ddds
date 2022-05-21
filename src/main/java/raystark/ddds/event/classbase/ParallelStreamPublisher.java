8f7c6b246edf7b2c776cbb197f6e9f91366e19d4
RayStarkMC <raystarkmc@gmail.com>
2022-05-22 00:29:11 +0900

    Add javadoc

--- src/main/java/raystark/ddds/event/classbase/ParallelStreamPublisher.java
+++ src/main/java/raystark/ddds/event/classbase/ParallelStreamPublisher.java
@@ -9,21 +9,38 @@ import raystark.ddds.event.Subscriber;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * 並列ストリームによりイベント処理を行うClassBasePublisher実装。
+ */
 public final class ParallelStreamPublisher implements ClassBasePublisher {
     private final Map<Class<? extends Event>, List<Subscriber<?>>> subscribersMap;
 
     private ParallelStreamPublisher(Map<Class<? extends Event>, List<Subscriber<?>>> subscribersMap) {
-        requireNonNull(subscribersMap);
-
         this.subscribersMap = subscribersMap;
     }
 
+    /**
+     * Publisherを生成します。
+     *
+     * <p>生成されたPublisherにはSubscriberが登録されていません。
+     * @return 新規生成されたPublisher
+     */
     public static ClassBasePublisher create() {
         return new ParallelStreamPublisher(API.Map());
     }
 
+    /**
+     * イベントを購読するサブスクライバに引き渡します。
+     *
+     * <p>どのSubscriberの処理もStreamライブラリが選択した任意のスレッドで実行される可能性があります。
+     * またSubscriberが共有状態を更新する場合その可視性は保証されないため、同期の責任はSubscriberにあります。
+     *
+     * @param event イベント
+     */
     @Override
     public void publish(Event event) {
+        requireNonNull(event);
+
         subscribersMap
             .get(event.getClass())
             .getOrElse(List.empty())
@@ -33,8 +50,15 @@ public final class ParallelStreamPublisher implements ClassBasePublisher {
             );
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <E extends Event> ClassBasePublisher subscribe(Class<E> clazz, Subscriber<E> subscriber) {
+        requireNonNull(clazz);
+        requireNonNull(subscriber);
+
         var newList = subscribersMap
             .getOrElse(clazz, List.empty())
             .append(subscriber);
