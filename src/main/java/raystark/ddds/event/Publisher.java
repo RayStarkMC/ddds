e79b9e72dd162ef1b07a8119f12bfa35188fe299
RayStarkMC <raystarkmc@gmail.com>
2022-05-21 23:45:21 +0900

    Fix javadoc

--- src/main/java/raystark/ddds/event/Publisher.java
+++ src/main/java/raystark/ddds/event/Publisher.java
@@ -5,7 +5,7 @@ package raystark.ddds.event;
  *
  * <p>publishメソッドに渡されたイベントは動的に型検査され、適切なサブスクライバに引き渡されます。
  *
- * @implSpec 実装クラスは自身へのSubscriberの登録方法を定義する必要があります。
+ * <p>実装クラスは自身へのSubscriberの登録方法を定義する必要があります。
  * @see Subscriber
  */
 public interface Publisher {
@@ -15,7 +15,7 @@ public interface Publisher {
      *
      * <p>このメソッドは副作用を生成します。
      *
-     * @implSpec 実装クラスはイベント処理がどのスレッドで行われるのか定義する必要があります。
+     * <p>実装クラスはイベント処理がどのスレッドで行われるのか定義する必要があります。
      * @param event イベント
      */
     void publish(Event event);
