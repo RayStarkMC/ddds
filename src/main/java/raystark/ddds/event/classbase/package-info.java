548b022949141654a8fef594e69d46401416d6a3
RayStarkMC <raystarkmc@gmail.com>
2022-05-22 00:54:39 +0900

    Add package-info

---
+++ src/main/java/raystark/ddds/event/classbase/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * クラスベースの出版購読イベントシステムを定義します。
+ *
+ * <p>クラスベースの出版購読イベントシステムでは、Publisherがイベントのclassオブジェクトを取得する事で
+ * 適切なSubscriberのhandleメソッドを呼び出します。
+ */
+package raystark.ddds.event.classbase;
