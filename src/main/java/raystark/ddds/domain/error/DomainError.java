16c9f07d8d5a32d5eaf900df5048c54052f363ea
RayStarkMC <raystarkmc@gmail.com>
2022-07-10 23:48:38 +0900

    Javadoc記述

--- src/main/java/raystark/ddds/domain/error/DomainError.java
+++ src/main/java/raystark/ddds/domain/error/DomainError.java
@@ -1,5 +1,26 @@
 package raystark.ddds.domain.error;
 
+/**
+ * ドメインレイヤーで何らかの処理が失敗した事を表す型です。
+ *
+ * <p>DomainErrorの実装者は次の様な形式のメソッドを提供するべきです。
+ *
+ * <pre> {@code
+ *     public static <T> Either<DomainError, T> withMessage(String message)
+ * }</pre>
+ *
+ * このメソッドは指定したメッセージを保持したDomainErrorを生成します。実装、及び利用例はNullDetectedを参照してください。
+ */
 public interface DomainError {
+
+    /**
+     * この失敗のメッセージを返します。
+     *
+     * <p>メッセージは開発者が呼んで理解できる形式の文字列です。
+     * このメソッドから返される文字列の形式は実装者の自由です。
+     * 利用者はこのメソッドから返される文字列を解析するプログラムを書くべきではありません。
+     *
+     * @return 失敗メッセージ
+     */
     String message();
 }
