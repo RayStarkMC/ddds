7c6cb56001f40e2ee1d6e792f4859b27cd0b7279
RayStarkMC <raystarkmc@gmail.com>
2022-07-10 23:54:01 +0900

    Javadoc記述

--- src/main/java/raystark/ddds/domain/error/NullDetected.java
+++ src/main/java/raystark/ddds/domain/error/NullDetected.java
@@ -4,8 +4,25 @@ import io.vavr.control.Either;
 
 import static java.util.Objects.isNull;
 
-public record NullDetected(String message) implements DomainError {
-    public static <T> Either<DomainError, T> withMessage(String message) {
+/**
+ * nullを検出した事を表します。
+ *
+ * <p>利用者はコンストラクタを利用しないでください。
+ *
+ * @param message メッセージ
+ */
+public record NullDetected(String message) implements DomainErrorDetected {
+
+    /**
+     * NullDetectedを生成します。
+     *
+     * <p>messageがnullの場合、その事実をメッセージに持つNullDetectedが生成されます。
+     *
+     * @param message メッセージ
+     * @return 指定したメッセージを持つNullDetected
+     * @param <T> 任意の型
+     */
+    public static <T> Either<DomainErrorDetected, T> withMessage(String message) {
         return isNull(message)?
             Either.left(new NullDetected("NullDetected creation failed. message is null.")):
             Either.left(new NullDetected(message));
